5b812275f871f42bd3cae47b85d887d9b4851ac4
Orestis Tsakiridis <otsakiridis@freemail.gr>
2022-11-18 16:48:19 +0200

    Adapting mesh frustum culling plumbing to use buffer metadata AABB

--- engine/gamesys/src/gamesys/components/comp_mesh.cpp
+++ engine/gamesys/src/gamesys/components/comp_mesh.cpp
@@ -28,6 +28,7 @@
 #include <dlib/dstrings.h>
 #include <dlib/transform.h>
 #include <dmsdk/dlib/vmath.h>
+#include <dmsdk/dlib/intersection.h> // for dmIntersection::Frustum
 #include <gameobject/gameobject_ddf.h>
 #include <graphics/graphics.h>
 #include <render/render.h>
@@ -738,6 +739,64 @@ namespace dmGameSystem
             break;
         }
     }
+
+
+    void metadataAabbToPoints(const void* data, dmBuffer::ValueType valueType, dmVMath::Point3& minPoint, dmVMath::Point3& maxPoint) {
+
+    }
+
+    static void RenderListFrustumCulling(dmRender::RenderListVisibilityParams const &params)
+    {
+        DM_PROFILE("Mesh");
+
+        MeshWorld* mesh_world = (MeshWorld*)params.m_UserData;
+
+
+        const dmIntersection::Frustum frustum = *params.m_Frustum;
+        uint32_t num_entries = params.m_NumEntries;
+        for (uint32_t i = 0; i < num_entries; ++i)
+        {
+            dmRender::RenderListEntry* entry = &params.m_Entries[i];
+            MeshComponent* component_p = (MeshComponent*)entry->m_UserData;
+
+            dmGameSystem::BufferResource* br = GetVerticesBuffer(component_p, component_p->m_Resource);
+            dmVMath::Point3 boundsMin, boundsMax;
+
+            void* data;
+            uint32_t count;
+            dmBuffer::ValueType valueType;
+            dmBuffer::Result r = dmBuffer::GetMetaData(br->m_Buffer, dmHashString64("AABB"), &data, &count, &valueType );
+            if (r == dmBuffer::RESULT_METADATA_NOT_EXIST) {
+                // no bounding box available - no culling
+                entry->m_Visibility = dmRender::VISIBILITY_FULL; // TODO - or shall we just "continue"
+                continue;
+            } else
+            if (r != dmBuffer::RESULT_OK) {
+                dmLogError("Error getting AABB for mesh buffer");
+                continue;
+            }
+
+            metadataAabbToPoints(data, valueType, boundsMin, boundsMax);
+
+            // get center of bounding box in local coords
+            float center_x = (boundsMax.getX() + boundsMin.getX())/2;
+            float center_y = (boundsMax.getY() + boundsMin.getY())/2;
+            float center_z = (boundsMax.getZ() + boundsMin.getZ())/2;
+            dmVMath::Point3 center_local(center_x, center_y, center_z);
+            dmVMath::Point3 corner_local(boundsMax.getX(), boundsMax.getY(), boundsMax.getZ());
+
+            // transform to world coords
+            dmVMath::Vector4 center_world = component_p->m_World * center_local;
+            dmVMath::Vector4 corner_world = component_p->m_World * corner_local;
+
+            float radius =  Vectormath::Aos::length(corner_world - center_world);
+
+            bool intersect = dmIntersection::TestFrustumSphere(frustum, center_world, radius, true);
+            entry->m_Visibility = intersect ? dmRender::VISIBILITY_FULL : dmRender::VISIBILITY_NONE;
+        }
+
+    }
+
 
     static void RenderListDispatch(dmRender::RenderListDispatchParams const &params)
     {
